1d799b002456e538b98149d1c5228d37c8916563
javieraibardev <JavierAibarDev@gmail.com>
2022-08-16 16:57:56 +0200

    Modal Traffic Light times working

--- Assets/Scripts/TrafficLightController.cs
+++ Assets/Scripts/TrafficLightController.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider))]
 public class TrafficLightController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public GameObject trafficLightPanel;
+    public TrafficLightUIController trafficLightUIPanel;
+    public float red, yellow, green;
+    
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, 100.0f) && hit.transform.name == gameObject.name)
+            {
+                print(hit.transform.name);
+                trafficLightPanel.SetActive(true);
+                trafficLightUIPanel.SetValues(red, yellow, green);
+                trafficLightUIPanel.SetSender(gameObject.GetComponent<TrafficLightController>());
+            }
+        }
+    }
+
+    internal void SetValues(float? redParam, float? yellowParam, float? greenParam)
+    {
+        if (redParam != null) red = redParam.Value; 
+        if (yellowParam != null) yellow = yellowParam.Value; 
+        if (greenParam != null) green = greenParam.Value; 
     }
 }
