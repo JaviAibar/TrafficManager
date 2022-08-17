d11bc0643e0f8e476c193343723e1996a99723f1
Javier Aibar <es.javi.es@gmail.com>
2022-08-17 03:37:52 +0200

    Traffic Lights fully working Time Control fully working (to this moment) Testing Completed

--- Assets/Scripts/TrafficLightController.cs
+++ Assets/Scripts/TrafficLightController.cs
@@ -3,15 +3,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider))]
+[RequireComponent(typeof(BoxCollider), typeof(SpriteRenderer))]
 public class TrafficLightController : MonoBehaviour
 {
+    public GameEngine gameEngine;
     public GameObject trafficLightPanel;
     public TrafficLightUIController trafficLightUIPanel;
     public float red, yellow, green;
-    
+    public float accumulativeYellow, accumulativeGreen;
+    public SpriteRenderer spriteRenderer;
+    [SerializeField] private float timer;
+    public float speed = 1;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        gameEngine = FindObjectOfType<GameEngine>();
+    }
+    private void Start()
+    {
+        timer = 0;
+    }
     void Update()
     {
+        timer += Time.deltaTime * speed;
+        if (timer % accumulativeYellow <= red)
+        {
+            spriteRenderer.sprite = gameEngine.trafficLightSprites[(int)GameEngine.trafficLightState.Red];
+        } else if (timer % accumulativeYellow <= accumulativeGreen) {
+            spriteRenderer.sprite = gameEngine.trafficLightSprites[(int)GameEngine.trafficLightState.Green];
+        }
+        else
+        {
+            spriteRenderer.sprite = gameEngine.trafficLightSprites[(int)GameEngine.trafficLightState.Yellow];
+        }
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
@@ -26,10 +51,18 @@ public class TrafficLightController : MonoBehaviour
         }
     }
 
-    internal void SetValues(float? redParam, float? yellowParam, float? greenParam)
+    public void SetValues(float? redParam, float? yellowParam, float? greenParam)
     {
         if (redParam != null) red = redParam.Value; 
         if (yellowParam != null) yellow = yellowParam.Value; 
-        if (greenParam != null) green = greenParam.Value; 
+        if (greenParam != null) green = greenParam.Value;
+
+        accumulativeGreen = red + green;
+        accumulativeYellow = accumulativeGreen + yellow;
+    }
+
+    public void SetSpeed(GameEngine.GameStates state)
+    {
+        speed = (float)state;
     }
 }
