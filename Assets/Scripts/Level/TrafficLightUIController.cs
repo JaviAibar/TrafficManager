6d7ceb3cdb2eeb5b1524cd3b0f7c8ed3770d3396
JaviAibar <es.javi.es@gmail.com>
2024-06-20 13:08:33 +0200

    Improved design TrafficLightPanel

--- Assets/Scripts/Level/TrafficLightUIController.cs
+++ Assets/Scripts/Level/TrafficLightUIController.cs
@@ -6,18 +6,17 @@ namespace Level
 {
     public class TrafficLightUIController : MonoBehaviour
     {
-
-        [SerializeField] private TMP_InputField redInputField;
-        [SerializeField] private TMP_InputField greenInputField;
-        [SerializeField] private TMP_InputField yellowInputField;
+        [SerializeField] private TextMeshProUGUI redText;
+        [SerializeField] private TextMeshProUGUI greenText;
+        [SerializeField] private TextMeshProUGUI yellowText;
 
         [SerializeField] private Slider redSlider;
         [SerializeField] private Slider greenSlider;
         [SerializeField] private Slider yellowSlider;
 
-        public int Red { get => (int)redSlider.value; set { redSlider.value = value; redInputField.text = value.ToString(); } }
-        public int Yellow { get => (int)yellowSlider.value; set { yellowSlider.value = value; yellowInputField.text = value.ToString(); } }
-        public int Green { get => (int)greenSlider.value; set { greenSlider.value = value; greenInputField.text = value.ToString(); } }
+        public int Red { get => (int)redSlider.value; set { redSlider.value = value; redText.text = value.ToString(); } }
+        public int Yellow { get => (int)yellowSlider.value; set { yellowSlider.value = value; yellowText.text = value.ToString(); } }
+        public int Green { get => (int)greenSlider.value; set { greenSlider.value = value; greenText.text = value.ToString(); } }
 
         public TrafficLightController Sender { get => sender; set => sender = value; }
 
@@ -25,10 +24,6 @@ namespace Level
 
         private void OnEnable()
         {
-            redInputField.onValueChanged.AddListener(RedInputFieldChange);
-            greenInputField.onValueChanged.AddListener(GreenInputFieldChange);
-            yellowInputField.onValueChanged.AddListener(YellowInputFieldChange);
-
             redSlider.onValueChanged.AddListener(RedSliderChange);
             greenSlider.onValueChanged.AddListener(GreenSliderChange);
             yellowSlider.onValueChanged.AddListener(YellowSliderChange);
@@ -36,10 +31,6 @@ namespace Level
 
         private void OnDisable()
         {
-            redInputField.onValueChanged.RemoveListener(RedInputFieldChange);
-            greenInputField.onValueChanged.RemoveListener(GreenInputFieldChange);
-            yellowInputField.onValueChanged.RemoveListener(YellowInputFieldChange);
-
             redSlider.onValueChanged.RemoveListener(RedSliderChange);
             greenSlider.onValueChanged.RemoveListener(GreenSliderChange);
             yellowSlider.onValueChanged.RemoveListener(YellowSliderChange);
@@ -53,33 +44,16 @@ namespace Level
         }
         void RedSliderChange(float value)
         {
-            redInputField.text = value.ToString();
+            redText.text = value.ToString();
         }
 
         void GreenSliderChange(float value)
         {
-            greenInputField.text = value.ToString();
+           greenText.text = value.ToString();
         }
         void YellowSliderChange(float value)
         {
-            yellowInputField.text = value.ToString();
-        }
-        void RedInputFieldChange(string value)
-        {
-            float.TryParse(value, out float valueFloat);
-            redSlider.value = valueFloat;
-        }
-
-        void GreenInputFieldChange(string value)
-        {
-            float.TryParse(value, out float valueFloat);
-            greenSlider.value = valueFloat;
-
-        }
-        void YellowInputFieldChange(string value)
-        {
-            float.TryParse(value, out float valueFloat);
-            yellowSlider.value = valueFloat;
+           yellowText.text = value.ToString();
         }
 
         public void Cancel() => gameObject.SetActive(false);
