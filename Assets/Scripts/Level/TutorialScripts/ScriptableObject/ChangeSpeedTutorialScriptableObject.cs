ab7f5cd9ed9be0bd2e92a07dfa8c8db75ed114c9
JaviAibar <es.javi.es@gmail.com>
2024-07-24 16:35:04 +0200

    Created 2 ScriptableObject events: ShowMessage and ChangeSpeed

---
+++ Assets/Scripts/Level/TutorialScripts/ScriptableObject/ChangeSpeedTutorialScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using Level;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChangeSpeedTutorial", menuName = "ScriptableObjects/ChangeSpeedTutorial")]
+[System.Serializable]
+public class ChangeSpeedTutorialScriptableObject : TutorialEvent
+{
+    public GameEngine.GameSpeed speed;
+    public override void Execute()
+    {
+        GameEngine.Instance.Speed = speed;
+        Completed = true;
+    }
+}
---
+++ Assets/Scripts/Level/TutorialScripts/ScriptableObject/ShowMessageTutorialScriptableObject.cs
@@ -0,0 +1,32 @@
+﻿using Level;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Localization.Settings;
+
+[CreateAssetMenu(fileName = "ShowMessageTutorial", menuName = "ScriptableObjects/ShowMessageTutorial")]
+
+[System.Serializable]
+public class ShowMessageTutorialScriptableObject : TutorialEvent
+{
+    public string messageIndex;
+    private GameObject tutorialWindow;
+    private TMP_Text tutorialText;
+
+    public override void Awake()
+    {
+        base.Awake();
+        TutorialController tutController = FindAnyObjectByType<TutorialController>();
+        tutorialWindow = tutController.TutorialWindow;
+        tutorialText = tutController.TutorialText;
+        Completed = false;
+    }
+
+    public override void Execute()
+    {
+        if (StopTime)
+            GameEngine.Instance.Speed = GameEngine.GameSpeed.Paused;
+
+        tutorialWindow.SetActive(true);
+        tutorialText.text = LocalizationSettings.StringDatabase.GetLocalizedString("Tutorial Messages", messageIndex);
+    }
+}
