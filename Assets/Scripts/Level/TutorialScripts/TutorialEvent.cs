185c35a42d23c19be20a32a95a730cfae7615246
JaviAibar <es.javi.es@gmail.com>
2024-07-24 16:34:19 +0200

    Created TutorialEvent that will be parent of every ScriptableObject TutorialEvent

---
+++ Assets/Scripts/Level/TutorialScripts/TutorialEvent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public abstract class TutorialEvent : ScriptableObject
+{
+    public float time;
+    public bool asap;
+    public bool Asap => asap;
+    public float Time => time;
+    public bool stopTime;
+    public bool StopTime => stopTime;
+
+    public bool Completed { get; protected set; } = false;
+
+    public bool initiated = false;
+
+    public virtual void Awake()
+    {
+        initiated = true;
+    }
+
+    public abstract void Execute();
+}
